da1e61cf38eb2d38ef09348732cda3b1bceb08ff
Vitaly Zdanevich <zdanevich.vitaly@ya.ru>
2016-01-12 20:35:15 +0300

    comments, trying to fix

--- src/chromium/background.js
+++ src/chromium/background.js
@@ -1,6 +1,6 @@
 // ui button
 chrome.tabs.onUpdated.addListener(function(tabId,  changeInfo,  tab){
-  if (~tab.url.indexOf('vk.com'))
+  if (~tab.url.indexOf('vk.com')) // if we at vk
     chrome.pageAction.show(tabId) // make ui button active (colored and functional)
 })
 
@@ -17,12 +17,19 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
 function fill(request, url) {
 // 	if (i < 20 && info.title.length > 0) {
         chrome.tabs.create({url:url}); // open tab
-        chrome.tabs.executeScript(null, {file:"contentScript.js"});
-		chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-          chrome.tabs.sendMessage(tabs[0].id, {request: request}, function(response){
-            alert('fill tab here2');
+
+        // TODO looks like contentScript not injecting into new tab
+        setTimeout(function(){ // wait before newly opened tab loads
+          // insert contentScript into page
+          chrome.tabs.executeScript(null, {file:"contentScript.js"});
+        },5000);
+        setTimeout(function(){
+          chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {request: request}, function(response){
+              alert('background.js: callback of sending message to contentScript');
+            });
           });
-        });
+        }, 6000)
 // 		setTimeout(function(){
 // 			i++;
 // 			fill(request);
--- src/chromium/contentScript.js
+++ src/chromium/contentScript.js
@@ -1,14 +1,17 @@
-alert('contentscript');
+alert('contentscript start');
 
 var title = document.getElementById('item_name').value;
 var desc = document.getElementById('item_description').value;
 var price = document.getElementById('item_price').value;
 
 chrome.runtime.sendMessage({'title':title,'desc':desc,'price':price}, null);
+alert('contentscript: after sending message');
 
+// TODO this not reveice the call
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
 //   if (request) {
-      alert('fill tab here');
+      alert('contentscript: message received');
+      // open modal for adding new product
       document.getElementsByClassName('market_add_item_lnk t_r')[0].children[0].click();
 //   }
 })
