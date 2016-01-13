fff55ca0c71975fbe618c87d5a100ea5adf50546
Vitaly Zdanevich <zdanevich.vitaly@ya.ru>
2016-01-13 10:13:41 +0300

    chromium some move forward

--- src/chromium/background.js
+++ src/chromium/background.js
@@ -6,11 +6,21 @@ chrome.tabs.onUpdated.addListener(function(tabId,  changeInfo,  tab){
 
 // onclick - insert & execute contentScript
 chrome.pageAction.onClicked.addListener(function(tab) {
-  chrome.tabs.executeScript(null, {file:"contentScript.js"})
+  chrome.tabs.executeScript(null, {file:"contentScript.js"});
+  setTimeout(function(){
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, {xxx:'xxx'}, function(response){
+        alert('background.js: callback of sending message to contentScript');
+      });
+    });
+  }, 3000)
 })
 
 // getted message from contentScript with data of form
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
+  alert('getted message from contentScript: \n\n' + request.title + '\n\n' +
+                                                    request.desc + '\n\n' +
+                                                    request.price + '\n\n');
   fill(request, sender.tab.url);
 })
 
@@ -18,14 +28,14 @@ function fill(request, url) {
 // 	if (i < 20 && info.title.length > 0) {
         chrome.tabs.create({url:url}); // open tab
 
-        // TODO looks like contentScript not injecting into new tab
         setTimeout(function(){ // wait before newly opened tab loads
           // insert contentScript into page
           chrome.tabs.executeScript(null, {file:"contentScript.js"});
         },5000);
         setTimeout(function(){
+          // TODO this can not send message to contentScript of new tab
           chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, {request: request}, function(response){
+            chrome.tabs.sendMessage(tabs[0].id, {request:request}, function(response){
               alert('background.js: callback of sending message to contentScript');
             });
           });
--- src/chromium/contentScript.js
+++ src/chromium/contentScript.js
@@ -1,17 +1,18 @@
-alert('contentscript start');
+alert('contentscript injected');
 
-var title = document.getElementById('item_name').value;
-var desc = document.getElementById('item_description').value;
-var price = document.getElementById('item_price').value;
-
-chrome.runtime.sendMessage({'title':title,'desc':desc,'price':price}, null);
-alert('contentscript: after sending message');
-
-// TODO this not reveice the call
+// TODO this not reveice the call at new tab
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
 //   if (request) {
       alert('contentscript: message received');
+
       // open modal for adding new product
-      document.getElementsByClassName('market_add_item_lnk t_r')[0].children[0].click();
+      if (request.title) {
+        document.getElementsByClassName('market_add_item_lnk t_r')[0].children[0].click();
+      } else { // script only started and now on original page - grub data
+        var title = document.getElementById('item_name').value;
+        var desc = document.getElementById('item_description').value;
+        var price = document.getElementById('item_price').value;
+        chrome.runtime.sendMessage({'title':title,'desc':desc,'price':price}, null);
+      }
 //   }
 })
